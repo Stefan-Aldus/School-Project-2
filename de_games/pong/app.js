bc0d0ea31de09380c8776b539e0869b3189a421c
FlorisBreemer <9018644@student.zadkine.nl>
2022-12-09 09:06:42 +0100

    begin pong

---
+++ de_games/pong/app.js
@@ -0,0 +1,30 @@
+let paddle_left = document.getElementById("paddle_left")
+let paddle_right = document.getElementById("paddle_right")
+let position_left = 0
+let position_right = 0
+let stepsize = 10;
+let gridheight = 480;
+
+
+window.addEventListener("keydown", function(event) {
+console.log(event.key);
+
+if (event.key == "s"&& position_left < gridheight) {
+   position_left += stepsize
+    paddle_left.style.top = position_left + "px"
+ }
+
+ if (event.key == "w" && position_left > 0) {
+   position_left -= stepsize
+    paddle_left.style.top = position_left + "px"
+ }
+
+ if (event.key == "ArrowDown"&& position_right < gridheight) {
+   position_right += stepsize
+    paddle_right.style.top = position_right + "px"
+ }
+
+ if (event.key == "ArrowUp" && position_right > 0) {
+   position_right -= stepsize
+    paddle_right.style.top = position_right + "px"
+ }})
